062fd1e1ff4792495010635b378d29200cdc67de
coderitual <skowronkow@hotmail.com>
2017-08-26 22:55:12 +0200

    style: prettify

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const createSandbox = () => {
     clearInterval
   };
 
-  Object.defineProperty(sandbox, 'module', {
+  Object.defineProperty(sandbox, "module", {
     enumerable: true,
     configurable: false,
     writable: false,
@@ -23,41 +23,43 @@ const createSandbox = () => {
   return sandbox;
 };
 
-const exposeRemoteServices = (sandbox) => {
+const exposeRemoteServices = sandbox => {
   if (process.env.GLITCHD_TOKEN === undefined) {
-    return
+    return;
   }
 
-  const services = require('glitchd-client-node');
+  const services = require("glitchd-client-node");
   sandbox.Buffer = Buffer;
-  Object.defineProperty(sandbox, 'glitchd', {
+  Object.defineProperty(sandbox, "glitchd", {
     value: Object.create(null, {
       items: {
-        value: new services.ItemsStore('services.js13kgames.com:13312', process.env.GLITCHD_TOKEN)
+        value: new services.ItemsStore(
+          "services.js13kgames.com:13312",
+          process.env.GLITCHD_TOKEN
+        )
       }
     })
   });
 };
 
-require('fs').readFile('./public/shared.js', 'utf8', (err, shared) => {
-  require('fs').readFile('./public/server.js', 'utf8', (err, code) => {
+require("fs").readFile("./public/shared.js", "utf8", (err, shared) => {
+  require("fs").readFile("./public/server.js", "utf8", (err, code) => {
     if (err) {
-      throw err
+      throw err;
     }
 
-    const
-      express = require('express'),
-      app     = express(),
-      server  = require('http').Server(app),
-      io      = require('socket.io')(server),
+    const express = require("express"),
+      app = express(),
+      server = require("http").Server(app),
+      io = require("socket.io")(server),
       sandbox = createSandbox();
 
-    require('vm').runInNewContext(shared + '\n' + code, sandbox);
-    io.on('connection', sandbox.module.exports);
-    app.set('port', (process.env.PORT || 3000));
-    app.use(express.static('public'));
-    server.listen(app.get('port'), () => {
-      console.log('Server started at port: ' + app.get('port'));
+    require("vm").runInNewContext(shared + "\n" + code, sandbox);
+    io.on("connection", sandbox.module.exports);
+    app.set("port", process.env.PORT || 3000);
+    app.use(express.static("public"));
+    server.listen(app.get("port"), () => {
+      console.log("Server started at port: " + app.get("port"));
     });
   });
 });
--- public/server.js
+++ public/server.js
@@ -11,14 +11,11 @@ var users = [];
  * @param {User} user
  */
 function findOpponent(user) {
-	for (var i = 0; i < users.length; i++) {
-		if (
-			user !== users[i] && 
-			users[i].opponent === null
-		) {
-			new Game(user, users[i]).start();
-		}
-	}
+  for (var i = 0; i < users.length; i++) {
+    if (user !== users[i] && users[i].opponent === null) {
+      new Game(user, users[i]).start();
+    }
+  }
 }
 
 /**
@@ -26,7 +23,7 @@ function findOpponent(user) {
  * @param {User} user
  */
 function removeUser(user) {
-	users.splice(users.indexOf(user), 1);
+  users.splice(users.indexOf(user), 1);
 }
 
 /**
@@ -35,75 +32,71 @@ function removeUser(user) {
  * @param {User} user2
  */
 function Game(user1, user2) {
-	this.user1 = user1;
-	this.user2 = user2;
+  this.user1 = user1;
+  this.user2 = user2;
 }
 
 /**
  * Start new game
  */
-Game.prototype.start = function () {
-	this.user1.start(this, this.user2);
-	this.user2.start(this, this.user1);
-}
+Game.prototype.start = function() {
+  this.user1.start(this, this.user2);
+  this.user2.start(this, this.user1);
+};
 
 /**
  * Is game ended
  * @return {boolean}
  */
-Game.prototype.ended = function () {
-	return this.user1.guess !== GUESS_NO && this.user2.guess !== GUESS_NO;
-}
+Game.prototype.ended = function() {
+  return this.user1.guess !== GUESS_NO && this.user2.guess !== GUESS_NO;
+};
 
 /**
  * Final score
  */
-Game.prototype.score = function () {
-	if (
-		this.user1.guess === GUESS_ROCK && this.user2.guess === GUESS_SCISSORS ||
-		this.user1.guess === GUESS_PAPER && this.user2.guess === GUESS_ROCK ||
-		this.user1.guess === GUESS_SCISSORS && this.user2.guess === GUESS_PAPER
-	) {
-		this.user1.win();
-		this.user2.lose();
-	} else if (
-		this.user2.guess === GUESS_ROCK && this.user1.guess === GUESS_SCISSORS ||
-		this.user2.guess === GUESS_PAPER && this.user1.guess === GUESS_ROCK ||
-		this.user2.guess === GUESS_SCISSORS && this.user1.guess === GUESS_PAPER
-	) {
-		this.user2.win();
-		this.user1.lose();
-	} else {
-		this.user1.draw();
-		this.user2.draw();
-	}
-}
+Game.prototype.score = function() {
+  if (
+    (this.user1.guess === GUESS_ROCK && this.user2.guess === GUESS_SCISSORS) ||
+    (this.user1.guess === GUESS_PAPER && this.user2.guess === GUESS_ROCK) ||
+    (this.user1.guess === GUESS_SCISSORS && this.user2.guess === GUESS_PAPER)
+  ) {
+    this.user1.win();
+    this.user2.lose();
+  } else if (
+    (this.user2.guess === GUESS_ROCK && this.user1.guess === GUESS_SCISSORS) ||
+    (this.user2.guess === GUESS_PAPER && this.user1.guess === GUESS_ROCK) ||
+    (this.user2.guess === GUESS_SCISSORS && this.user1.guess === GUESS_PAPER)
+  ) {
+    this.user2.win();
+    this.user1.lose();
+  } else {
+    this.user1.draw();
+    this.user2.draw();
+  }
+};
 
 /**
  * User session class
  * @param {Socket} socket
  */
 function User(socket) {
-	this.socket = socket;
-	this.game = null;
-	this.opponent = null;
-	this.guess = GUESS_NO;
+  this.socket = socket;
+  this.game = null;
+  this.opponent = null;
+  this.guess = GUESS_NO;
 }
 
 /**
  * Set guess value
  * @param {number} guess
  */
-User.prototype.setGuess = function (guess) {
-	if (
-		!this.opponent ||
-		guess <= GUESS_NO ||
-		guess > GUESS_SCISSORS
-	) {
-		return false;
-	}
-	this.guess = guess;
-	return true;
+User.prototype.setGuess = function(guess) {
+  if (!this.opponent || guess <= GUESS_NO || guess > GUESS_SCISSORS) {
+    return false;
+  }
+  this.guess = guess;
+  return true;
 };
 
 /**
@@ -111,69 +104,69 @@ User.prototype.setGuess = function (guess) {
  * @param {Game} game
  * @param {User} opponent
  */
-User.prototype.start = function (game, opponent) {
-	this.game = game;
-	this.opponent = opponent;
-	this.guess = GUESS_NO;
-	this.socket.emit("start");		
+User.prototype.start = function(game, opponent) {
+  this.game = game;
+  this.opponent = opponent;
+  this.guess = GUESS_NO;
+  this.socket.emit("start");
 };
 
 /**
  * Terminate game
  */
-User.prototype.end = function () {
-	this.game = null;
-	this.opponent = null;
-	this.guess = GUESS_NO;
-	this.socket.emit("end");
+User.prototype.end = function() {
+  this.game = null;
+  this.opponent = null;
+  this.guess = GUESS_NO;
+  this.socket.emit("end");
 };
 
 /**
  * Trigger win event
  */
-User.prototype.win = function () {
-	this.socket.emit("win", this.opponent.guess);
+User.prototype.win = function() {
+  this.socket.emit("win", this.opponent.guess);
 };
 
 /**
  * Trigger lose event
  */
-User.prototype.lose = function () {
-	this.socket.emit("lose", this.opponent.guess);
+User.prototype.lose = function() {
+  this.socket.emit("lose", this.opponent.guess);
 };
 
 /**
  * Trigger draw event
  */
-User.prototype.draw = function () {
-	this.socket.emit("draw", this.opponent.guess);
+User.prototype.draw = function() {
+  this.socket.emit("draw", this.opponent.guess);
 };
 
 /**
  * Socket.IO on connect event
  * @param {Socket} socket
  */
-module.exports = function (socket) {
-	var user = new User(socket);
-	users.push(user);
-	findOpponent(user);
-	
-	socket.on("disconnect", function () {
-		console.log("Disconnected: " + socket.id);
-		removeUser(user);
-		if (user.opponent) {
-			user.opponent.end();
-			findOpponent(user.opponent);
-		}
-	});
+module.exports = function(socket) {
+  var user = new User(socket);
+  users.push(user);
+  findOpponent(user);
 
-	socket.on("guess", function (guess) {
-		console.log("Guess: " + socket.id);
-		if (user.setGuess(guess) && user.game.ended()) {
-			user.game.score();
-			user.game.start();
-		}
-	});
+  socket.on("disconnect", function() {
+    console.log("Disconnected: " + socket.id);
+    removeUser(user);
+    if (user.opponent) {
+      user.opponent.end();
+      findOpponent(user.opponent);
+    }
+  });
 
-	console.log("Connected: " + socket.id);
+  socket.on("guess", function(guess) {
+    console.log("Guess: " + socket.id);
+    if (user.setGuess(guess) && user.game.ended()) {
+      user.game.score();
+      user.game.start();
+    }
+  });
+
+  console.log("Connected: " + socket.id);
 };
