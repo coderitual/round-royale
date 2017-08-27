e08679799b6cd4f66d2f33c08d7e0e86ed12b53a
coderitual <skowronkow@hotmail.com>
2017-08-27 11:33:32 +0200

    feat: prepare canvas for mobile

--- public/client.js
+++ public/client.js
@@ -1,6 +1,12 @@
 (function () {
 'use strict';
 
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
+canvas.width = canvas.clientWidth;
+canvas.height = canvas.clientHeight;
+
+console.log(canvas.width, canvas.height);
 
 }());
