a7b463727b5571028723c77d9e8f4f044e536772
coderitual <skowronkow@hotmail.com>
2017-09-09 00:21:20 +0200

    feat: add some server stuff

--- public/server.js
+++ public/server.js
@@ -1,1 +1,85 @@
-"use strict";var createUser=function(e){},users=new Set,index=function(e){console.log("connect: "+e.id);var n=createUser(e);users.add(n),e.on("disconnect",function(){console.log("disconnect: "+e.id),users.delete(n)}),e.on("player_data",function(e){console.log("player_data"),console.dir(e)})};module.exports=index;
+'use strict';
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var createUser = function createUser(socket) {
+  return new function User() {
+    _classCallCheck(this, User);
+
+    this.socket = socket;
+    this.game = null;
+    this.player = null;
+  }();
+};
+
+var createGame = function createGame() {
+  var users = new Set();
+
+  var createPlayer = {
+    x: 0,
+    y: 0,
+    vx: 0,
+    vy: 0,
+    ax: 0,
+    ay: 0
+  };
+
+  var loop = function loop() {};
+
+  var interval = setInterval(loop, 30);
+  var destroy = function destroy() {
+    return clearInterval(interval);
+  };
+
+  return {
+    addUser: function addUser(user) {
+      users.add(user);
+      user.player = createPlayer();
+    },
+
+    get usersCount() {
+      return users.size;
+    },
+    destroy: function destroy() {
+      clearInterval(interval);
+    },
+    userDataDidReceive: function userDataDidReceive(data) {}
+  };
+};
+
+var findOrCreateGame = function findOrCreateGame() {
+  var game = [].concat(_toConsumableArray(games)).find(function (game) {
+    return game.usersCount < 2;
+  });
+  if (!game) {
+    game = createGame();
+    games.add(game);
+  }
+  return game;
+};
+
+var users = new Set();
+var games = new Set();
+
+var index = (function (socket) {
+  console.log("connect: " + socket.id);
+  var user = createUser(socket);
+  users.add(user);
+  var game = findOrCreateGame();
+
+  user.game = game;
+
+  socket.on("disconnect", function () {
+    console.log("disconnect: " + socket.id);
+    users.delete(user);
+  });
+
+  socket.on("player_data", function (data) {
+    console.log("player_data");
+    console.dir(data);
+  });
+});
+
+module.exports = index;
--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,67 @@
-const createUser = socket => {};
 
-const createGame = () => {};
+const createUser = socket => {
+  return new class User {
+    socket = socket;
+    game = null;
+    player = null;
+  };
+};
+
+const createGame = () => {
+  const users = new Set();
+
+  const createPlayer = ({
+    x: 0,
+    y: 0,
+    vx: 0,
+    vy: 0,
+    ax: 0,
+    ay: 0
+  });
+
+  const loop = () => {
+
+  }
+
+  const interval = setInterval(loop, 30);
+  const destroy = () => clearInterval(interval);
+
+  return {
+    addUser(user) {
+      users.add(user);
+      user.player = createPlayer();
+    },
+    get usersCount() {
+      return users.size;
+    },
+    destroy() {
+      clearInterval(interval);
+    },
+    userDataDidReceive(data) {
+
+    }
+  }
+};
+
+const findOrCreateGame = () => {
+  let game = [...games].find(game => game.usersCount < 2);
+  if(!game) {
+    game = createGame();
+    games.add(game);
+  }
+  return game;
+}
 
 const users = new Set();
+const games = new Set();
 
 export default socket => {
   console.log(`connect: ${socket.id}`);
   const user = createUser(socket);
   users.add(user);
+  const game = findOrCreateGame();
+
+  user.game = game;
 
   socket.on("disconnect", () => {
     console.log(`disconnect: ${socket.id}`);
